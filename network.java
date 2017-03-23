52a439575922cf5ccf75a9df03beb2dc34f0a8d4
mbentz <mbentz@ufl.edu>
2017-03-23 13:27:08 -0400

    Added clarification comments and cleaned up code

--- network.java
+++ network.java
@@ -3,27 +3,21 @@ import java.net.*;
 import java.io.*;
 
 /**
-* The <code>Network</code> relays a packet and acknowledgement
-* between a sender and receiver.
-* Lossy channel with bit errors is emulated using PASS, CORRUPT and DROP
-* PASS = 0.5%
-* CORRUPT = 0.25%
-* DROP = 0.25%
-*/
+ * The <code>network</code> is a single instance meant to simulate a lossy channel with bit errors
+ * by PASSing, CORRUPTing or DROPing messages between the <code>sender</code>
+ * and <code>receiver</code>. The network creates a <code>Handler</code> thread
+ * for both sender and receiver.
+ */
 public class network{
-	private static network instance = null;
-	private static final String TAG = "Network> ";
-	private static ServerSocket listener;
-	private static Handler[] handlers;
-	private static HashMap<Handler,Handler> destinationMap;
-	private static int PORT;
-	private static int clientCount = 0;
+	private static network instance = null;					// single instance of
+	private static final String TAG = "Network> ";			// TAG used for debugging
+	private static ServerSocket listener;					// ServerSocket
+	private static Handler[] handlers;						// Array to hold the Handlers
+	private static HashMap<Handler,Handler> destinationMap;	// Map to create a pairing between the Handlers
+	private static int PORT;								// PORT
+	private static int clientCount = 0;						// # of clients connected i.e. receiver & sender
 
-	private static void initStorage() {
-		destinationMap = new HashMap<>();
-		handlers = new Handler[2];
-	}
-
+	// Synchronized getInstance() allows the network to be shared by both Handlers
 	public synchronized static network getInstance(){
 		if (instance == null){
 			instance = new network();
@@ -33,24 +27,29 @@ public class network{
 
 	// TODO fix the ordering of the try catch statements
 	public static void main(String[] args){
-		// Initialize the variables
-		initStorage();
-		PORT = Integer.parseInt(args[0]);
+		int PORT = Integer.parseInt(args[0]);
 		System.out.println(TAG + "Network opening port: " + PORT);
+
+		// Initializing variables
+		destinationMap = new HashMap<>();
+		handlers = new Handler[2];
+
 		try{
+			// Setup ServerSocket at the given port
 			listener = new ServerSocket(PORT);
 		}catch(Exception exception){
 			System.out.println(TAG + "Unable to attach to port");
 			System.exit(1);
 		}
 
+		// TODO check this condition
 		while (true){
 			try{
-				// If there is a receiver and sender
+				// If there is a receiver and sender create the pairing
 				if (clientCount == 2){
 					createPairing();
 				}
-				// Wait until connection attempt is made
+				// Wait until connection attempt is made and accept
 				Socket client = listener.accept();
 				// Create a Handler and pass the client socket
 				Handler clientHandler = new Handler(client);
@@ -66,6 +65,7 @@ public class network{
 		}
 	}
 
+	// Creates a pairing between the Receiver Handler and Sender Handler
 	private static void createPairing(){
 		Handler receiver, sender = null;
 
@@ -74,12 +74,15 @@ public class network{
 		sender = handlers[1];
 
 		// Create the mapping from receiver to sender
+		// and vice versa
 		destinationMap.put(receiver,sender);
 		destinationMap.put(sender,receiver);
 	}
 
+	// Sends the message to the destination given the source Handler
+	// and chooses, at random, to: PASS, CORRUPT, or DROP the message.
 	public void sendToDestination(Message message, Handler source){
-		System.out.println(TAG + "getting ready to send to destination");
+//		System.out.println(TAG + "getting ready to send to destination");
 		// Get the destination
 		Handler destination = destinationMap.get(source);
 
@@ -87,124 +90,50 @@ public class network{
 		Random random = new Random();
 		double value = random.nextDouble();
 
-		// TODO uncomment other messages
 		// Choose an operation
-		if (value <= .50){
+		if (value <= .50){ // 50% probability to PASS
 			passData(message, destination);
-		} else if (value <= .75){
-//			passData(message, destination);
+			printMessage(message,"PASS");
+		} else if (value <= .75){ // 25% probability to CORRUPT
 			 corruptData(message, destination);
-		} else{
-//			passData(message,destination);
-//			corruptData(message, destination);
+			 printMessage(message,"CORRUPT");
+		} else{ // 25% probability to DROP
 			dropData();
+			printMessage(message,"DROP");
 		}
 	}
 
-	/**
-	* Drop packet or ACK, sendPacket a DROP message to the sender
-	* Use ACK2 for DROP message, i.e., ACK with seq# 2 to
-	* emulate a fake timeout
-	*/
+	// Drops the message and sends ACK2 to the Sender Handler
 	public void dropData(){
 		Handler senderHandler = handlers[1];
 		// Create an ACK
 		ACK dropAck = new ACK((byte)2,(byte)0);
 		// Send the ack back to the sender
 		senderHandler.sendMessage(dropAck);
-		System.out.println(TAG + "Data was dropped enroute. Sending ACK(2)");
-//		if(source == handlers[1]) {
-//			source.sendMessage(dropAck);
-//		}
+//		System.out.println(TAG + "Data was dropped enroute. Sending ACK(2)");
 	}
 
 
-	/**
-	 * Sends the data to the receiver
-	 */
+	// Passes the data to the destination handler
 	public void passData(Message message, Handler destination){
 		destination.sendMessage(message);
-		System.out.println(TAG + "Sending packet to: " + destination.getLinkedClient());
+//		System.out.println(TAG + "Sending packet to: " + destination.getLinkedClient());
 	}
 
-	/**
-	 * Add 1 to the checksum field
-	 */
+	// Corrupts the data being passed by incrementing the checksum
 	public void corruptData(Message message, Handler destination){
 		// Clone the message so that it's original contents aren't changed
 		Message copy = message.clone();
 		// Corrupt the data
 		copy.corruptData();
 		destination.sendMessage(copy);
-		System.out.println(TAG + "Corrupted the packet's data going to: " + destination.getLinkedClient());
+//		System.out.println(TAG + "Corrupted the packet's data going to: " + destination.getLinkedClient());
 	}
 
 
-	/**
-	* When a packet is received it is printed out
-	* packet type, ID (no ID for ACK) and the content it picks
-	*/
-	private void printPacket(Packet packet){
-		// Received: Packet0, 5, DROP
-		// Received ACK1, PASS
+	// Prints the message being sent and the corresponding
+	// operation taken on the message
+	private void printMessage(Message message, String operation){
+		System.out.println(message.toString() + operation);
 	}
 }
-
-
-//	public void sendToDestination(Packet packet, Handler source){
-//		// Get the destination
-//		Handler destination = destinationMap.get(source);
-//
-//		// Generate a random value
-//		Random random = new Random();
-//		double value = random.nextDouble();
-//
-//		// Choose an operation
-//		if (value <= .50){
-//			passData(packet, destination);
-//		} else if (value <= .75){
-//			passData(packet, destination);
-////			 corruptData(packet, destination);
-//		} else{
-//			passData(packet, destination);
-////			dropData(source);
-//		}
-//
-//	}
-//
-//	public void sendToDestination(ACK ack, Handler source){
-//		// Get the destination
-//		Handler destination = destinationMap.get(source);
-//
-//		// Generate a random value
-//		Random random = new Random();
-//		double value = random.nextDouble();
-//
-//		// Choose an operation
-//		if (value <= .50){
-//			passData(ack, destination);
-//		} else if (value <= .75){
-//			corruptData(ack, destination);
-//		} else{
-//			dropData(source);
-//		}
-//	}
-
-//	/**
-//	* Sends the data to the receiver
-//	*/
-//	public void passData(Packet packet, Handler destination){
-//		destination.sendPacket(packet);
-//		System.out.println(TAG + "Sending packet to: " + destination.getLinkedClient());
-//	}
-//
-//	/**
-//	* Add 1 to the checksum field
-//	*/
-//	public void corruptData(Packet packet, Handler destination){
-//		// Corrupt the data
-//		packet.corruptData();
-//		// Send the packet to the destination
-//		destination.sendPacket(packet);
-//		System.out.println(TAG + "Corrupted the packet's data going to: " + destination.getLinkedClient());
-//	}
